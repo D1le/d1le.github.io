65cbbb0413ee61317a11c0cf0a5bd26be0064c24
Alexey Lapin <alexey-lapin@protonmail.com>
2019-09-02 22:54:23 +0300

    travis fix

---
+++ .travis/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e -u
+
+yarn run clean
+gatsby build --prefix-paths
+gh-pages \
+    --dist public \
+    --branch master \
+    --repo "https://${GITHUB_TOKEN}@$github.com/${TRAVIS_REPO_SLUG}.git"
+    --user "Travis CI <deploy@travis-ci.org>"
